6b1e41b81391a1be82b8d3817c30e85f3b398d01
madhukaweerakoon <madhuka.20200777@iit.ac.lk>
2021-07-09 23:48:59 +0530

    PlayerSelection

---
+++ src/Player.java
@@ -0,0 +1,30 @@
+public class Player {
+    private String playerName;
+    private int age;
+    private String playerType;
+    private double statistics;
+
+    public Player(String playerName, int age, String playerType, double statistics){
+        this.playerName = playerName;
+        this.age = age;
+        this.playerType = playerType;
+        this.statistics = statistics;
+    }
+
+    void playerAverage(){
+
+    }
+
+    void getPlayerDetails(){
+
+    }
+
+    void getBestPerformance(){
+
+    }
+
+    void getPlayerType(){
+
+    }
+}
+
